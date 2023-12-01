d2fb352952bae5d309787af7fd24ade3a6783ba5
Ritesh Kumar Singh <Ritesh.Singh@target.com>
2023-12-01 17:31:34 +0530

    Day 1

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -2,16 +2,28 @@ package day01
 
 import println
 import readInput
+import java.lang.Exception
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+            (it.find { it.isDigit() }.toString() + it.findLast { it.isDigit() }).toInt()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val map = mapOf(
+            "one" to 1, "two" to 2, "three" to 3, "four" to 4,
+            "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9,
+            "1" to 1, "2" to 2, "3" to 3, "4" to 4, "5" to 5, "6" to 6,
+            "7" to 7, "8" to 8, "9" to 9
+        )
+        return input.sumOf {
+            val first = map[it.findAnyOf(map.keys)!!.second].toString()
+            val second = map[it.findLastAnyOf(map.keys)!!.second].toString()
+            (first + second).toInt()
+        }
     }
-
 
     val input = readInput("/day01/Day01")
     part1(input).println()
