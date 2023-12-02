f7d44729e4692b94586f989fa744abc48c2eb17c
Ritesh Kumar Singh <Ritesh.Singh@target.com>
2023-12-02 12:52:08 +0530

    Day 2

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -3,115 +3,50 @@ package day02
 import println
 import readInput
 
-fun main() {
+private data class Cube(
+    val red: Int = 0,
+    val blue: Int = 0,
+    val green: Int = 0
+)
 
-    data class Cube(
-        val red: Int = 0,
-        val blue: Int = 0,
-        val green: Int = 0
-    )
-
-    fun part1(input: List<String>): Int {
-        val map = mutableMapOf<Int, List<Cube>>()
-        input.forEach {
-            val bagAndCubes = it.split(":")
-            val game = bagAndCubes[0].substringAfter("Game").trim().toInt()
-            val cubes: List<Cube> = bagAndCubes[1].split(";")
-                .map { it.trim() }
-                .map {
-                    val cubes = it.split(",").map { it.trim() }
+private fun List<String>.parse(): Map<Int, List<Cube>> {
+    return this.map { it.split(":").map { it.trim() } }
+        .associateBy({
+            it[0].substringAfter("Game").trim().toInt()
+        }, {
+            it[1].split(";")
+                .map { cubes -> cubes.split(",").map { it.trim() } }
+                .map { cubes ->
                     var cube = Cube()
                     cubes.forEach {
-                        if (it.contains("blue")) {
-                            val num = it.filter { it.isDigit() }.toInt()
-                            cube = cube.copy(blue = num)
-                        }
-                        if (it.contains("red")) {
-                            val num = it.filter { it.isDigit() }.toInt()
-                            cube = cube.copy(red = num)
-                        }
-                        if (it.contains("green")) {
-                            val num = it.filter { it.isDigit() }.toInt()
-                            cube = cube.copy(green = num)
+                        when {
+                            it.contains("blue") -> cube = cube.copy(blue = it.filter { it.isDigit() }.toInt())
+                            it.contains("red") -> cube = cube.copy(red = it.filter { it.isDigit() }.toInt())
+                            it.contains("green") -> cube = cube.copy(green = it.filter { it.isDigit() }.toInt())
+                            else -> {}
                         }
                     }
                     cube
                 }
+        })
+}
 
-            map[game] = cubes
-        }
+fun main() {
 
-        var total = 0
-
-        val RED = 12
-        val GREEN = 13
-        val BLUE = 14
-
-        map.forEach {
-            val result = it.value.all {
-                it.red <= RED && it.blue <= BLUE && it.green <= GREEN
-            }
-            if (result) {
-                total += it.key
-            }
-        }
-
-        return total
+    fun part1(input: List<String>): Int {
+        return input.parse()
+            .filter {
+                it.value.all { it.red <= 12 && it.blue <= 14 && it.green <= 13 }
+            }.map { it.key }
+            .sum()
     }
 
     fun part2(input: List<String>): Int {
-        val map = mutableMapOf<Int, List<Cube>>()
-        input.forEach {
-            val bagAndCubes = it.split(":")
-            val game = bagAndCubes[0].substringAfter("Game").trim().toInt()
-            val cubes: List<Cube> = bagAndCubes[1].split(";")
-                .map { it.trim() }
-                .map {
-                    val cubes = it.split(",").map { it.trim() }
-                    var cube = Cube()
-                    cubes.forEach {
-                        if (it.contains("blue")) {
-                            val num = it.filter { it.isDigit() }.toInt()
-                            cube = cube.copy(blue = num)
-                        }
-                        if (it.contains("red")) {
-                            val num = it.filter { it.isDigit() }.toInt()
-                            cube = cube.copy(red = num)
-                        }
-                        if (it.contains("green")) {
-                            val num = it.filter { it.isDigit() }.toInt()
-                            cube = cube.copy(green = num)
-                        }
-                    }
-                    cube
-                }
-
-            map[game] = cubes
-        }
-
-        var total = 0
-
-        val RED = 12
-        val GREEN = 13
-        val BLUE = 14
-
-        map.forEach {
-            var red = it.value.maxOf { it.red }
-            var blue = it.value.maxOf { it.blue }
-            var green = it.value.maxOf { it.green }
-
-            total += (red * blue * green)
-        }
-
-        return total
+        return input.parse()
+            .map {
+                it.value.maxOf { it.red } * it.value.maxOf { it.blue } * it.value.maxOf { it.green }
+            }.sum()
     }
-
-
-    val testInput = readInput("/day02/Day02_test")
-    part1(testInput).println()
-    part2(testInput).println()
-
-    println("-----------------------------------")
 
     val input = readInput("/day02/Day02")
     part1(input).println()
